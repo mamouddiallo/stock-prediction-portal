07cb9529ce51ea88bfb47db219d605bf830f64db
mamouddiallo <diallomamoud298@gmail.com>
2026-02-21 21:46:15 +0000

    commit front end

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <>
+        <footer className="footer py-3 my-3">
+            <hr className="border-bottom" />
+            <p className="text-light text-center"> &copy; 2026 - Build With ❤️ by Diallo Mamoudou</p>
+        </footer>
+    </>
+  )
+}
+export default Footer
---
+++ src/components/Main.jsx
@@ -0,0 +1,21 @@
+import Button from "./Button"
+
+const Main = () => {
+  return (
+    <div className="container">
+      <div className="p-5 text-center bg-light-dark rounded">
+        <h1 className="text-light">Stock Prediction Portal</h1>
+        <p className="text-light lead">
+          This stock prediction application utilizes machines learning techniques, 
+          specifically employing Keras, and LSTM model, integrated within the Django 
+          framework. It forcasts future stock prices by analyzing 100-day moving 
+          averages, essential indicators widely used by stock analysts to inform 
+          trading and investment decisions
+        </p>
+        <Button text="Login" class="btn-outline-info"/>
+      </div>
+    </div>
+  )
+}
+
+export default Main
